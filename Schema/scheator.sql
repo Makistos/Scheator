19ec66213a07eb589831b569d574e77a2bbbd178
Marko Poutiainen <marko.poutiainen@elektrobit.com>
2010-04-27 14:50:13 +0300

    Updated table schema

--- Schema/scheator.sql
+++ Schema/scheator.sql
@@ -1,23 +1,35 @@
 -- phpMyAdmin SQL Dump
--- version 3.2.2.1deb1
+-- version 3.3.2deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 01, 2010 at 09:36 PM
--- Server version: 5.1.37
--- PHP Version: 5.2.10-2ubuntu6.4
+-- Generation Time: Apr 27, 2010 at 02:49 PM
+-- Server version: 5.1.41
+-- PHP Version: 5.3.2-1ubuntu4
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `scheator`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Match`
+--
+
+CREATE TABLE IF NOT EXISTS `Match` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `match_no` int(11) NOT NULL,
+  `season` int(11) NOT NULL,
+  `hometeam` int(11) NOT NULL,
+  `awayteam` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `season` (`season`),
+  KEY `hometeam` (`hometeam`),
+  KEY `awayteam` (`awayteam`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -27,11 +39,12 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 CREATE TABLE IF NOT EXISTS `Season` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) NOT NULL,
+  `name` varchar(50) NOT NULL,
   `series` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `series` (`series`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  KEY `series` (`series`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -41,9 +54,10 @@ CREATE TABLE IF NOT EXISTS `Season` (
 
 CREATE TABLE IF NOT EXISTS `Series` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
 
 -- --------------------------------------------------------
 
@@ -53,6 +67,25 @@ CREATE TABLE IF NOT EXISTS `Series` (
 
 CREATE TABLE IF NOT EXISTS `Team` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) NOT NULL,
+  `name` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Match`
+--
+ALTER TABLE `Match`
+  ADD CONSTRAINT `Match_ibfk_1` FOREIGN KEY (`season`) REFERENCES `Season` (`id`),
+  ADD CONSTRAINT `Match_ibfk_2` FOREIGN KEY (`hometeam`) REFERENCES `Team` (`id`),
+  ADD CONSTRAINT `Match_ibfk_3` FOREIGN KEY (`awayteam`) REFERENCES `Team` (`id`);
+
+--
+-- Constraints for table `Season`
+--
+ALTER TABLE `Season`
+  ADD CONSTRAINT `Season_ibfk_1` FOREIGN KEY (`series`) REFERENCES `Series` (`id`);
+
